be38b46ce410719836e8b9a33a688ecf001f88c5
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2020-11-20 18:39:00 -0800

    tile and gamemanager base

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  [SerializeField] Tile[] tiles = new Tile[100]; // automatically make and add the tiles with appropriate tags
+  [SerializeField] GameObject map;
+
+   // walls_placed[]; // holds how many walls each player has on the board for rotations/movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       RandomizeBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public void RandomizeBoard() {
+    foreach(Tile t in tiles) {
+      t.Randomize();
+    }
+  }
+
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+   [SerializeField] GameObject[] tiles = new GameObject[14];
+   // add a openings array
+   //add a walled array
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+  // Update is called once per frame
+  void Update()
+    {
+        
+    }
+
+  public void Randomize() {
+    int tileSelection;
+    if(this.tag == "region1") {
+      tileSelection = Random.Range(8, 14);
+    }
+    else if(this.tag == "region2") {
+      tileSelection = Random.Range(4, 11);
+    }
+    else {
+      tileSelection = Random.Range(1, 6);
+    }
+    for(int i = 0; i < 14; i++) {
+      tiles[i].SetActive(false);
+    }
+    tiles[tileSelection].SetActive(true);
+    Debug.Log("Changed tiles visibilty");
+    // set openings
+    // randomly rotate a number of times
+  }
+
+  //add a function that rotates openings
+
+  //add a function that places/removes walls
+
+}
