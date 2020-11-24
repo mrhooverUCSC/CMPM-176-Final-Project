d5861939d87e43423479d7f13af6d1279d9fe076
ejrossma <ejrossma@ucsc.edu>
2020-11-23 19:34:43 -0800

    Added UI & a script to show the UI

---
+++ Assets/WorkButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WorkButton : MonoBehaviour
+{
+    public clickScript toggle;
+    void OnMouseDown()
+    {
+        toggle.ToggleVisibility();
+    }
+}
---
+++ Assets/clickScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //if tile is clicked on
+            //ToggleVisibility()
+    }
+
+    public void ToggleVisibility()
+    {
+        //var render = GameObject.GetComponentsInChildren<GameObject>();
+        //foreach (GameObject obj in render)
+        //{
+        //    if (obj.tag == "UI")
+        //    {
+        //        obj.activeSelf = !obj.activeSelf;
+        //    }
+        //}
+        var rotation = GameObject.Find("RotationUI");
+        rotation.SetActive(!rotation.activeSelf);
+    }
+}
