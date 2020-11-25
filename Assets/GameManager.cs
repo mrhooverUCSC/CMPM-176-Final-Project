0774ef786359d3366795a812b706b9e9a09b0ced
mrhooverUCSC <mrhoover@ucsc.edu>
2020-11-24 20:05:59 -0800

    Tile adjacency

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-  [SerializeField] Tile[] tiles = new Tile[100]; // automatically make and add the tiles with appropriate tags
-  [SerializeField] GameObject map;
+    Tile[] tiles; // automatically make and add the tiles with appropriate tags
+    [SerializeField] Tile test_tile;
+    Vector3 up = new Vector3(0, -1, 0); // distance comparisons
+    Vector3 up_right = new Vector3(-0.9f, -0.5f, 0.0f);
+    Vector3 down_right = new Vector3(-0.9f, +0.5f, 0.0f);
+    Vector3 down = new Vector3(0, 1, 0.0f);
+    Vector3 down_left = new Vector3(+0.9f, +0.5f, 0.0f);
+    Vector3 up_left = new Vector3(+0.9f, -0.5f, 0.0f);
 
+    Dictionary<Tile, Tile[]> adjacency= new Dictionary<Tile, Tile[]>();
    // walls_placed[]; // holds how many walls each player has on the board for rotations/movement
 
     // Start is called before the first frame update
     void Start()
     {
-       RandomizeBoard();
+        tiles = FindObjectsOfType<Tile>();
+        RandomizeBoard();
+        make_adjacency();
     }
 
     // Update is called once per frame
@@ -22,9 +31,48 @@ public class GameManager : MonoBehaviour
     }
 
   public void RandomizeBoard() {
-    foreach(Tile t in tiles) {
-      t.Randomize();
-    }
+      foreach(Tile t in tiles)
+      {
+         t.Randomize();
+      }
   }
 
+    private void make_adjacency()
+    {
+        foreach(Tile t in tiles){
+            Vector3 pos = t.transform.position;
+            Tile[] adj = new Tile[6];
+            foreach(Tile c in tiles)
+            {
+                Vector3 cpos = c.transform.position;
+               // Debug.Log(pos + " "+ cpos);
+                if(Vector3.Distance(pos, cpos + up) < .1)
+                {
+                    adj[0] = c;
+                }
+                else if (Vector3.Distance(pos, cpos + up_right) < .1)
+                {
+                    adj[1] = c;
+                    Debug.Log(true);
+                }
+                else if (Vector3.Distance(pos, cpos + down_right) < .1)
+                {
+                    adj[2] = c;
+                }
+                else if (Vector3.Distance(pos, cpos + down) < .1)
+                {
+                    adj[3] = c;
+                }
+                else if (Vector3.Distance(pos, cpos + down_left) < .1)
+                {
+                    adj[4] = c;
+                }
+                else if (Vector3.Distance(pos, cpos + up_left) < .1)
+                {
+                    adj[5] = c;
+                }
+            }
+            adjacency.Add(t, adj);
+        }
+    }
 }
