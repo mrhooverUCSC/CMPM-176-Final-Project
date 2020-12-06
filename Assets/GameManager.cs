d87f31b2fea85cb55945e9ec8ba27fd8cf20d82a
Felix Li <bli261@ucsc.edu>
2020-12-06 14:11:52 -0800

    debug

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -318,8 +318,4 @@ public class GameManager : MonoBehaviour
         MySingleton.Instance.selectedWall.transform.position = MySingleton.Instance.selectedWall.startLocation;
         MySingleton.Instance.selectedWall.isPlaced = false;
     }
-
-  public void wall_clicked() {
-    Debug.Log("Matt says Hi");
-  }
 }
